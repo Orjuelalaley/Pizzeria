07f5115afc0879cc3062270f21b83d64e9864383
Sebastián Orjuela <sebasorjuela@gmail.com>
2023-07-31 11:27:20 -0500

    Se agrego el primer RestController (GET)

---
+++ src/main/java/com/orjuelalaley/Pizzeria/web/controller/PizzaController.java
@@ -0,0 +1,28 @@
+package com.orjuelalaley.Pizzeria.web.controller;
+
+import com.orjuelalaley.Pizzeria.persistence.entity.PizzaEntity;
+import com.orjuelalaley.Pizzeria.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaController {
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaEntity>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(pizzaService.getAll());
+    }
+}
