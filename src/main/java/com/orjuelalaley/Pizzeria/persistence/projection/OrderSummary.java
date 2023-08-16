af6e687bd7bba71eff5da3decfef913d9f78963a
Sebastián Orjuela <sebasorjuela@gmail.com>
2023-08-16 16:31:03 -0500

    Fixing a little Bug with OrderSummary

--- src/main/java/com/orjuelalaley/Pizzeria/persistence/projection/OrderSummary.java
+++ src/main/java/com/orjuelalaley/Pizzeria/persistence/projection/OrderSummary.java
@@ -10,5 +10,5 @@ public interface OrderSummary {
     String getCustomerName();
     LocalDateTime getOrderDate();
     Double getOrderTotal();
-    String PizzaNames();
+    String getPizzaNames();
 }
